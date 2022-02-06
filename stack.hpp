6e507a722476845da99228826c05bed1899f9e12
ytaousi <yassir.taous@gmail.com>
2022-02-06 07:25:33 -0800

    ...

--- stack.hpp
+++ stack.hpp
@@ -3,6 +3,14 @@
 
 # include <vector>
 
+// STACK is implemented as an adaptive container it means it encapsulate
+//  an object of specifice container class as its underlying container 
+
+// The standard container classes  "vector", "list", "deque" can fullfill those requirements
+
+// By default if no container is defined for a stack class instantiation  , the standard container "deque"
+//  is used;
+
 namespace ft
 {
     template <class T, class Container = std::vector<T> >
@@ -12,48 +20,51 @@ namespace ft
 template <class T, class Container = std::vector<T>>
 class ft::stack
 {
+    public:
     // Type of the elements (T) first template parameter
     typedef T           value_type;
     // Type of underlying container second template parameter
     typedef Container   container_type;
     // an unsigned intergal type
     typedef size_t      size_type;
-
-    public:
-        // Members Fuctions
+    
+    // Members Fuctions
 
         // Constructor
-        explicit stack (const container_type& ctnr = container_type())
-        {
+    explicit stack (const container_type& ctnr = container_type())
+    {
 
-        }
+    }
 
-        bool empty() const
-        {
+    bool empty() const
+    {
 
-        }
+    }
         
-        size_type size() const
-        {
+    size_type size() const
+    {
 
-        }
+    }
         
-        value_type& top()
-        {
+    value_type& top()
+    {
 
-        }
+    }
         
-        const value_type& top() const
-        {
+    const value_type& top() const
+    {
 
-        }
+    }
 
-        void push (const value_type& val)
-        {
+    void push (const value_type& val)
+    {
             
-        }
+    }
 
-        void pop();
+    void pop()
+    {
+
+    }
 
 };
 
