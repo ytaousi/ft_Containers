df4b0a7c0b98f829cb4ed7cc22df6edd25fadd6f
ytaousi <46757421+ytaousi@users.noreply.github.com>
2022-02-06 16:06:47 +0100

    Add files via upload

---
+++ stack.hpp
@@ -0,0 +1,91 @@
+#ifndef STACK_HPP
+# define STACK_HPP
+
+# include <vector>
+
+namespace ft
+{
+    template <class T, class Container = std::vector<T> >
+    class stack;
+}
+
+template <class T, class Container = std::vector<T>>
+class ft::stack
+{
+    typedef T           value_type;
+
+    typedef Container   container_type;
+
+    typedef size_t      size_type;
+
+
+    explicit stack (const container_type& ctnr = container_type())
+    {
+
+    }
+
+    bool empty() const
+    {
+
+    }
+    
+    size_type size() const
+    {
+
+    }
+    
+    value_type& top()
+    {
+
+    }
+    
+    const value_type& top() const
+    {
+
+    }
+
+    void push (const value_type& val)
+    {
+        
+    }
+
+    void pop();
+
+};
+
+
+template <class T, class Container>
+bool operator== (const ft::stack<T,Container>& lhs, const ft::stack<T,Container>& rhs)
+{
+
+}
+
+template <class T, class Container>
+bool operator!= (const ft::stack<T,Container>& lhs, const ft::stack<T,Container>& rhs)
+{
+
+}
+
+template <class T, class Container>
+bool operator<  (const ft::stack<T,Container>& lhs, const ft::stack<T,Container>& rhs)
+{
+
+}
+
+template <class T, class Container>
+bool operator<= (const ft::stack<T,Container>& lhs, const ft::stack<T,Container>& rhs)
+{
+
+}
+template <class T, class Container>
+bool operator> (const ft::stack<T,Container>& lhs, const ft::stack<T,Container>& rhs)
+{
+
+}
+template <class T, class Container>
+bool operator>= (const ft::stack<T,Container>& lhs, const ft::stack<T,Container>& rhs)
+{
+
+}
+
+#endif
