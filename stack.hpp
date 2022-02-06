ec5869b1cdcbeeaf77a7f825e7417964ae8d5ec7
ytaousi <yassir.taous@gmail.com>
2022-02-06 07:15:36 -0800

    ...

--- stack.hpp
+++ stack.hpp
@@ -12,47 +12,52 @@ namespace ft
 template <class T, class Container = std::vector<T>>
 class ft::stack
 {
+    
     typedef T           value_type;
 
     typedef Container   container_type;
 
     typedef size_t      size_type;
 
+    public:
+        // Members Fuctions
 
-    explicit stack (const container_type& ctnr = container_type())
-    {
+        // Constructor
+        explicit stack (const container_type& ctnr = container_type())
+        {
 
-    }
+        }
 
-    bool empty() const
-    {
+        bool empty() const
+        {
 
-    }
-    
-    size_type size() const
-    {
-
-    }
-    
-    value_type& top()
-    {
-
-    }
-    
-    const value_type& top() const
-    {
-
-    }
-
-    void push (const value_type& val)
-    {
+        }
         
-    }
+        size_type size() const
+        {
 
-    void pop();
+        }
+        
+        value_type& top()
+        {
+
+        }
+        
+        const value_type& top() const
+        {
+
+        }
+
+        void push (const value_type& val)
+        {
+            
+        }
+
+        void pop();
 
 };
 
+// Non Member-Fuctions overloading
 
 template <class T, class Container>
 bool operator== (const ft::stack<T,Container>& lhs, const ft::stack<T,Container>& rhs)
