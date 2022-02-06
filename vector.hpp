0d3feed15672f6994c7eb376f52ca1add0e156b3
ytaousi <yassir.taous@gmail.com>
2022-02-06 11:26:19 -0800

    ..

---
+++ vector.hpp
@@ -0,0 +1,20 @@
+#ifndef VECTOR_HPP
+# define VECTOR_HPP
+
+# include <memory>
+
+// VECTOR is implemented as sequence
+
+namespace ft
+{
+    template <class T, class Alloc = std::allocator<T> >
+    class vector;
+}
+
+template <class T, class Alloc>
+class ft::vector
+{
+    public:
+
+};
+#endif
