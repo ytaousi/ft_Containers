7987037f4be1bcb58f48d9e42888a97b02fee0c4
ytaousi <yassir.taous@gmail.com>
2022-02-07 21:38:40 -0800

    ...

--- vector.hpp
+++ vector.hpp
@@ -16,13 +16,15 @@ class ft::vector
 {
     public:
         typedef T                                           value_type;
-        typedef size_t                                      size_type;
         typedef Alloc                                       allocator_type;
-        typedef typename allocator_type::reference          reference;
-        typedef typename allocator_type::const_reference    const_reference;
-        typedef typename allocator_type::pointer            pointer;
-        typedef typename allocator_type::const_pointer      const_pointer;
 
+        typedef value_type&                                 reference;
+        typedef const value_type&                           const_reference;
+        typedef value_type*                                 pointer;
+        typedef const value_type*                           const_pointer;
+
+        typedef size_t                                      size_type;
+        typedef ptrdiff_t                                   difference_type;
         // need to typedef the iterators type : check  documentation
 
     // need to define the default/fill/range constructors : done
@@ -290,7 +292,7 @@ class ft::vector
         allocator_type _alloc;
         size_type      _size;
         size_type      _capacity;
-        value_type     _array;
+        value_type*     _array; // pointer _array
 };
 
     // Non-member function's
