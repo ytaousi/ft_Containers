40ba25b5dfb8060d89107742b5059d9e19a07d77
ytaousi <yassir.taous@gmail.com>
2022-02-18 03:11:46 +0100

    nvalidiw lexam w nrj3ou lk xD

--- vector/main.cpp
+++ vector/main.cpp
@@ -5,13 +5,17 @@
 int main()
 {
     ft::vector<std::string> first(5, "Noob");
-    ft::vector<std::string> second(first);
+    //ft::vector<std::string> second(first);
+    //ft::vector<std::string> third = second;
 
     for (size_t i = 0; i < 5; i++)
         std::cout << first[i] << std::endl;
     std::cout << "*********************************" << std::endl;
-    for (size_t i = 0; i < 5; i++)
-       std::cout << second[i] << std::endl; 
+    // for (size_t i = 0; i < 5; i++)
+    //    std::cout << second[i] << std::endl;
+    // std::cout << "*********************************" << std::endl;
+    // for (size_t i = 0; i < 5; i++)
+    //    std::cout << third[i] << std::endl; 
     
     return (0);
 }
